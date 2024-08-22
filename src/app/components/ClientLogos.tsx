e7e1da4805f934d35e1fdab2a31588671f41ff12
RizWaaN3024 <rizwaanans786@gmail.com>
2024-08-23 02:01:28 +0530

    Added ClientLogos Component

---
+++ src/app/components/ClientLogos.tsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import Image from 'next/image'
+
+interface Logo {
+    src: string;
+    alt: string;
+    width: number;
+    height: number;
+}
+
+
+const logos: Logo[] = [
+    {
+        src: '/clientLogos/screenfixer.png',
+        alt: 'screenfixer',
+        width: 96,
+        height: 39
+    },
+    {
+        src: '/clientLogos/walsos.png',
+        alt: 'walsos',
+        width: 153,
+        height: 31
+    },
+    {
+        src: '/clientLogos/moengage.png',
+        alt: 'moengage',
+        width: 168,
+        height: 26
+    },
+    {
+        src: '/clientLogos/hashgrowth.png',
+        alt: 'hashgrowth',
+        width: 122,
+        height: 31
+    },
+    {
+        src: '/clientLogos/century.png',
+        alt: 'century',
+        width: 170,
+        height: 26
+    },
+    {
+        src: '/clientLogos/sectona.png',
+        alt: 'sectona',
+        width: 126,
+        height: 34
+    },
+]
+
+const ClientLogos: React.FC = () => {
+  return (
+    <section className='bg-blushPink'>
+        <div className='lg:container mx-auto px-4'>
+            <div className='flex items-center justify-around px-4 py-12'>
+                {logos.map((logo, index) => (
+                    <div
+                      key={index}
+                      className={`flex max-w-full aspect-[${logo.width}/${logo.height}]`}
+                    >
+                        <Image 
+                            src={logo.src}
+                            alt={logo.alt}
+                            width={logo.width}
+                            height={logo.height}
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default ClientLogos
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Hero from "@components/Hero"
+import ClientLogos from "@components/ClientLogos";
 
 export default function Home() {
   return (
     <main className="">
       <Hero />
+      <ClientLogos />
     </main>
   );
 }
