029f4544cd84d7725c4849806bad65fea23407fd
RizWaaN3024 <rizwaanans786@gmail.com>
2024-08-23 01:22:27 +0530

    Modified Button

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,17 +1,22 @@
 import React from 'react'
 
+type ButtonVariant = "primary" | "secondary"
 type ButtonProps = {
     children: React.ReactNode;
     onClick?: () => void;
     href: string,
-    variant?: 'primary' | 'secondary'; 
+    variant?: ButtonVariant; 
     className?: string; 
 }
 
-const Button: React.FC<ButtonProps> = ({ children, onClick, variant = 'primary', href, className }) => {
-    const variantStyles = variant === 'primary' ? 'bg-btnPrimary text-white' : 'bg-transparent';
+const BUTTON_VARIANT_MAPPER: Record<ButtonVariant, string> = {
+  "primary": "bg-btnPrimary text-white",
+  "secondary": "bg-transparent"
+};
 
-    const combinedStyles = `${variantStyles} ${className}`
+const Button: React.FC<ButtonProps> = ({ children, onClick, variant = 'primary', href, className }) => {
+    // const variantStyles = variant === 'primary' ? 'bg-btnPrimary text-white' : 'bg-transparent';
+    const combinedStyles = `${BUTTON_VARIANT_MAPPER[variant]} ${className}`
   return (
     <a href={href} className={combinedStyles}>
       {children}
